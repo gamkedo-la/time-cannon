dd113a18129c45351942dbb85926e406596aca95
Michael Fewkes <mdfewkes@gmail.com>
2020-07-26 16:40:56 -0600

    More robust MusicTimeKeeper

--- Assets/Scripts/General/MusicTimeKeeper.cs
+++ Assets/Scripts/General/MusicTimeKeeper.cs
@@ -6,15 +6,21 @@ using UnityEngine.Audio;
 public class MusicTimeKeeper : MonoBehaviour
 {
 	public AudioMixer mixer;
-	public float minTempoMult = 0.9f;
-	public float maxTempoMult = 1.1f;
+	public float tempoMultRange = 0.2f;
+	public List<AudioSource> sources;
 
-	private AudioMixerGroup[] musicGroup;
-	private AudioMixerGroup[] sfxGroup;
+	private bool hasSources = false;
+	private float minTempoMult;
+	private float maxTempoMult;
+	private List<AudioSource> deadSources = new List<AudioSource>(0);
 
 	private void Start() {
-		musicGroup = mixer.FindMatchingGroups("Music");
-		sfxGroup = mixer.FindMatchingGroups("SFX");
+		if (sources.Count >= 1) {
+			hasSources = true;
+		}
+
+		minTempoMult = 1 - tempoMultRange;
+		maxTempoMult = 1 + tempoMultRange;
 	}
 
 	private void Update() {
@@ -26,8 +32,37 @@ public class MusicTimeKeeper : MonoBehaviour
 			pitchDampen = 2f;
 		}
 
-		mixer.SetFloat("MusicSpeedMult", Lerp(0f, minTempoMult, 2f, maxTempoMult, Mathf.Abs(TimeKeeper.instance.fakeTimePace)) * speedDampen);
+		if (hasSources) {
+			if (TimeKeeper.instance.fakeTimePace < 0) speedDampen *= -1;
+			foreach (AudioSource source in sources) {
+				if (source == null) {
+					deadSources.Add(source);
+					continue;
+				}
+				source.pitch = Lerp(0f, minTempoMult, 2f, maxTempoMult, Mathf.Abs(TimeKeeper.instance.fakeTimePace)) * speedDampen;
+			}
+		} else {
+			mixer.SetFloat("MusicSpeedMult", Lerp(0f, minTempoMult, 2f, maxTempoMult, Mathf.Abs(TimeKeeper.instance.fakeTimePace)) * speedDampen);
+		}
 		mixer.SetFloat("MusicPitchMult", Lerp(0f, maxTempoMult, 2f, minTempoMult, Mathf.Abs(TimeKeeper.instance.fakeTimePace)) * pitchDampen);
+
+		if (deadSources.Count >= 1) {
+			foreach (AudioSource source in deadSources) {
+				sources.Remove(source);
+			}
+			deadSources.Clear();
+			if (sources.Count == 0) hasSources = false;
+		}
+	}
+
+	public void AddSource(AudioSource newSource) {
+		sources.Add(newSource);
+		hasSources = true;
+	}
+
+	public void RemoveSource(AudioSource oldSource) {
+		sources.Remove(oldSource);
+		if (sources.Count == 0) hasSources = false;
 	}
 
 	private float Lerp(float x1, float y1, float x2, float y2, float x) {
