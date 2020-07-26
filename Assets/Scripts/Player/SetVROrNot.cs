9ba17542d8eb3e0dd0c388ca130c4b63438624e8
Gamkedo <chris@gamkedo.com>
2020-07-25 22:52:22 -0700

    Player prefab in both scenes now autodetects if running in VR or Editor/webGL and uses appropriate camera/control/UI setup. Known bug in modern city scene: ships aren't being removed when shot (but count and show score?)

---
+++ Assets/Scripts/Player/SetVROrNot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVROrNot : MonoBehaviour {
+    public bool forceVR = false;
+
+    public GameObject webController;
+    public GameObject vrController;
+
+    void Awake() {
+        if (Application.platform == RuntimePlatform.WebGLPlayer) {
+            forceVR = false;
+        } else if (Application.platform == RuntimePlatform.Android) {
+            forceVR = true;
+        }
+        vrController.SetActive(forceVR);
+        webController.SetActive(!forceVR);
+    }
+}
