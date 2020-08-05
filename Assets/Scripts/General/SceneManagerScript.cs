34658e7507cd64a79601f7c1ceb8b91dcd1e47f3
Barış Köklü <30669414+bariskoklu@users.noreply.github.com>
2020-08-05 22:44:47 +0300

    Restarting scene functionality added. It is binded to the Key "K".

---
+++ Assets/Scripts/General/SceneManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public static SceneManagerScript Instance { get; private set; } = null;
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            RestartScene();
+        }   
+    }
+    public void RestartScene()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+}
