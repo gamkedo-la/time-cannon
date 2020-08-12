8acb7c1c7c1d03c16b7c5aaf33343590df495136
Barış Köklü <30669414+bariskoklu@users.noreply.github.com>
2020-08-13 00:58:36 +0300

    Revert "Revert "First version of the round summary screen is added.""
    This reverts commit ea5fb66a26507c1f777b749fe0981f9bb8078660.

---
+++ Assets/Scripts/Scriptable Objects/IntListSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IntListSO", menuName = "ScriptableObject/IntListSO", order = 3)]
+public class IntListSO : ScriptableObject
+{
+    public List<int> value;
+}
---
+++ Assets/Scripts/Scriptable Objects/IntSO.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "IntSO", menuName = "ScriptableObject/IntSO", order = 2)]
+public class IntSO : ScriptableObject
+{
+    public int value;
+}
