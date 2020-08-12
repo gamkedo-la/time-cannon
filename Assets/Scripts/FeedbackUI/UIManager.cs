c79686425fc46d73c66a21ae26df2cb75b892f71
Barış Köklü <30669414+bariskoklu@users.noreply.github.com>
2020-08-13 00:37:29 +0300

    First version of the round summary screen is added.

---
+++ Assets/Scripts/FeedbackUI/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject Player;
+    private SetVROrNot setVROrNotScript;
+    [SerializeField] private GameObject RoundSummaryUIWebGL;
+    void Start()
+    {
+        setVROrNotScript = Player.GetComponent<SetVROrNot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (!setVROrNotScript.forceVR)
+            {
+                RoundSummaryUIWebGL.GetComponent<FillRoundSummaryInfo>().DrawScreen();
+                RoundSummaryUIWebGL.SetActive(true);
+            }
+            //else //TODO
+            //{
+            //    RoundSummaryUIVR.SetActive(true);
+            //}
+
+        }
+    }
+}
--- Assets/Scripts/Targets/ExplodeChainReact.cs
+++ Assets/Scripts/Targets/ExplodeChainReact.cs
@@ -17,6 +17,10 @@ public class ExplodeChainReact : MonoBehaviour
     private int numSegments = 100;
     private LineRenderer lineRenderer;
 
+    //UI Elements for round summary screen
+    [SerializeField] private IntListSO ScoreList;
+    [SerializeField] private IntListSO NumberOfTargetsList;
+    [SerializeField] private IntSO TotalNumberOfTargetHits;
     private void Start()
     {
         blastVFX = Resources.Load("Explosion5m") as GameObject;
@@ -90,8 +94,22 @@ public class ExplodeChainReact : MonoBehaviour
 
         GameObject pointGO = GameObject.Instantiate(pointPopper, transform.position, transform.rotation);
         PointScaleFadeDie psfdScript = pointGO.GetComponent<PointScaleFadeDie>();
-        psfdScript.SetText(chainDepth * 100, chainDepth);
-        ScoreKeeper.instance.AddScore(100 * chainDepth);
+        int score = chainDepth * 100;
+        psfdScript.SetText(score, chainDepth);
+        ScoreKeeper.instance.AddScore(score);
+
+        // UI Elements Handling
+        TotalNumberOfTargetHits.value += 1;
+        if (chainDepth == 1)
+        {
+            ScoreList.value.Add(score);
+            NumberOfTargetsList.value.Add(chainDepth);
+        }
+        else if (chainDepth > 1)
+        {
+            ScoreList.value[ScoreList.value.Count -1] += score;
+            NumberOfTargetsList.value[NumberOfTargetsList.value.Count - 1] += 1;
+        }
 
         GameObject.Instantiate(blastVFX, transform.position, transform.rotation);
 
