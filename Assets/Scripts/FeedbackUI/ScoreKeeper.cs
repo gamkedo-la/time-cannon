2ca79ce4b82a94a6bf3587ecc72ea5cb964d98e4
KornelGM <KORNik_k@wp.pl>
2020-10-17 15:08:49 +0200

    Score label reacts to points being gained (and the more points the stronger the reaction)

--- Assets/Scripts/FeedbackUI/ScoreKeeper.cs
+++ Assets/Scripts/FeedbackUI/ScoreKeeper.cs
@@ -5,19 +5,41 @@ public class ScoreKeeper : MonoBehaviour
 {
     public static ScoreKeeper instance;
 
+    [SerializeField] private float scaleDecay = 1f;
+    [SerializeField] private float scaleIncreasePerPoint = 0.005f;
+
     private TextMeshProUGUI scoreLabel;
+    private Transform scoreLabelParent;
+    private float scaleFactor = 1;
     private int scoreNow = 0;
+
+    private const int MinScale = 1;
 
     void Start()
     {
         instance = this;
         scoreLabel = GameObject.Find("Score Label").GetComponent<TextMeshProUGUI>();
+        scoreLabelParent = scoreLabel.transform.parent;
         AddScore(0);
+    }
+
+    void Update()
+    {
+        AnimateLabel();
     }
 
     public void AddScore(int points)
     {
+        scaleFactor += points * scaleIncreasePerPoint;
         scoreNow += points;
         scoreLabel.text = "Score: "+ scoreNow;
+    }
+
+    private void AnimateLabel()
+    {
+        scaleFactor -= scaleDecay * Time.deltaTime;
+        scaleFactor = scaleFactor < MinScale ? MinScale : scaleFactor;
+
+        scoreLabelParent.transform.localScale = Vector3.one * scaleFactor;
     }
 }
