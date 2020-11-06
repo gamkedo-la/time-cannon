0d67b5d87bccb5c242c2094f6e4f2cf347d903d9
Rikard Åkesson <rikard.m.akesson@gmail.com>
2020-11-06 14:18:46 -0800

    Add transitions between main menu and levels

---
+++ Assets/Scripts/MainMenu/TimeCannonPlayerPrefs.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class TimeCannonPlayerPrefs
+{
+    public static void SetTimePeriod(TimePeriod timePeriod)
+    {
+        PlayerPrefs.SetInt("stageNow", (int)timePeriod);
+    }
+}
