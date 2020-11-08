51a10f74ec13a734efe8fd300490e825e4fac677
Gamkedo <chris@gamkedo.com>
2020-11-08 04:15:31 -0800

    Working on VR zoom / precision aim solution

--- Assets/Scripts/Player/SceneChangeCheat.cs
+++ Assets/Scripts/Player/SceneChangeCheat.cs
@@ -17,22 +17,20 @@ public class SceneChangeCheat : MonoBehaviour
 
         // debug controls to switch or reload scenes in VR
         if (Input.GetButtonDown("Fire3Joy")) {
-            SceneManager.LoadScene(0);
+            //SceneManager.LoadScene(0); // menu - no use loading in VR until working in VR
+            // SceneManager.LoadScene(4); // chaos - error loading in VR? black screen
         }
         if (Input.GetButtonDown("Fire4Joy")) {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(1); // city
         }
         if (Input.GetButtonDown("Fire5Joy")) {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(2); // ocean
         }
         if (Input.GetButtonDown("Fire6Joy")) {
-            SceneManager.LoadScene(3);
+            SceneManager.LoadScene(3); // countryside
         }
         if (Input.GetButtonDown("Fire7Joy")) {
-            SceneManager.LoadScene(4);
-        }
-        if (Input.GetButtonDown("Fire8Joy")) {
-            SceneManager.LoadScene(5);
+            // no action? button 7 might not be reachable by defualt Unity input mapping
         }
     }
 }
---
+++ Assets/Scripts/Player/VRAim.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRAim : MonoBehaviour
+{
+    public GameObject gunObject;
+    public GameObject zoomHand;
+
+    private Vector3 zoomAimOrigin = Vector3.zero;
+    private Quaternion zoomAimRot = Quaternion.identity;
+
+    private Vector3 restoreAimOrigin = Vector3.zero;
+    private Quaternion restoreAimRot = Quaternion.identity;
+
+    private Vector3 aimHandOrigin = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restoreAimOrigin = gunObject.transform.localPosition;
+        restoreAimRot = gunObject.transform.localRotation;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire3Joy")) {
+            gunObject.transform.localPosition = restoreAimOrigin;
+            gunObject.transform.localRotation = restoreAimRot;
+            Vector3 zoomHandNow = transform.InverseTransformPoint(zoomHand.transform.position);
+            gunObject.transform.rotation *= Quaternion.AngleAxis((aimHandOrigin.x - zoomHandNow.x)*-45.0f, Vector3.up);
+            gunObject.transform.rotation *= Quaternion.AngleAxis((aimHandOrigin.y - zoomHandNow.y) * 30.0f, Vector3.right);
+        }
+
+        if (Input.GetButtonDown("Fire3Joy")) {
+            aimHandOrigin = transform.InverseTransformPoint(zoomHand.transform.position);
+            zoomAimOrigin = gunObject.transform.position;
+            zoomAimRot = gunObject.transform.rotation;
+        } else if (Input.GetButtonUp("Fire3Joy")) {
+            gunObject.transform.localPosition = restoreAimOrigin;
+            gunObject.transform.localRotation = restoreAimRot;
+        }
+    }
+}
