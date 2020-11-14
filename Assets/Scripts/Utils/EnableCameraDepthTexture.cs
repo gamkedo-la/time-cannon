516e72a80c3f2b3dec2e6b0e4d011d132be56403
KornelGM <KORNik_k@wp.pl>
2020-11-14 10:52:58 +0100

    Potential fixes for particles not rendering in VR

---
+++ Assets/Scripts/Utils/EnableCameraDepthTexture.cs
@@ -0,0 +1,15 @@
+﻿/**
+ * Description: Enabled camera depth texture rendering.
+ * Authors: Kornel
+ * Copyright: © 2020 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+
+public class EnableCameraDepthTexture : MonoBehaviour
+{
+	void Start( )
+	{
+		GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+	}
+}
