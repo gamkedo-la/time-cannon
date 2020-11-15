6f5a1feac56e307df1e86c9cc685a35898f6368d
Gamkedo <chris@gamkedo.com>
2020-11-15 11:18:34 -0800

    Dropping in new plane models to view in-scene, propellor spin script updated to support different axis

--- Assets/Scripts/General/SimpleSpin.cs
+++ Assets/Scripts/General/SimpleSpin.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 public class SimpleSpin : MonoBehaviour
 {
     public float rate = 10.0f;
-    void Update()
-    {
-        transform.Rotate(Vector3.right, rate * TimeKeeper.instance.fakeTimeDelta);
+
+    public enum spinAxis {onX,onY,onZ};
+
+    public spinAxis spinOn = spinAxis.onX;
+
+    private Vector3 onAxis;
+
+    void Start() {
+        switch (spinOn) {
+            case spinAxis.onX:
+                onAxis = Vector3.right;
+                break;
+            case spinAxis.onY:
+                onAxis = Vector3.up;
+                break;
+            case spinAxis.onZ:
+                onAxis = Vector3.forward;
+                break;
+        }
+    }
+    void Update() {
+        transform.Rotate(onAxis, rate * TimeKeeper.instance.fakeTimeDelta);
     }
 }
