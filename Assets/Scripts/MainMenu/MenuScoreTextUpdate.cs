f3239e31c9c8577abe8620dd45523b9b3263e2f0
Gamkedo <chris@gamkedo.com>
2020-11-21 20:59:15 -0800

    Start of menu score summing script

---
+++ Assets/Scripts/MainMenu/MenuScoreTextUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuScoreTextUpdate : MonoBehaviour
+{
+    private readonly string[] sceneNames = { "Desert", "Ocean", "Countryside", "Chaos" };
+    public TextMeshProUGUI[] sceneLabels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i< sceneLabels.Length; i++) {
+            sceneLabels[i].text = sceneNames[i]+"\nTotal: 0";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
