9a51362a78d3236ddff5a972402c172888167ec4
Rikard Åkesson <rikard.m.akesson@gmail.com>
2020-11-29 02:49:21 -0800

    Make main menu to level transition feel more time travel-esque

--- Assets/Scripts/MainMenu/LevelTransitionController.cs
+++ Assets/Scripts/MainMenu/LevelTransitionController.cs
@@ -1,15 +1,26 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelTransitionController : MonoBehaviour
 {
+    public float FinalParticleSimulationSpeed = 10;
+    public float FinalParticleXScale = 10;
+    public float FinalParticleYScale = 10;
+    public float FinalTitleXScale = 10;
+    public float FinalTitleYScale = 0.1f;
     public int TransitionTimeInSeconds = 1;
+    public ParticleSystem TimeParticles;
+    public Transform Title;
 
+    private float initialParticleSimulationSpeed;
+    private float initialParticleXScale;
+    private float initialParticleYScale;
+    private float initialTitleXScale;
+    private float initialTitleYScale;
     private bool inTransition = false;
     private string nextSceneName;
-    private Image fullScreenBlackRectangle;
+    private Image fullScreenRectangle;
 
     private void Start()
     {
@@ -24,15 +35,42 @@ public class LevelTransitionController : MonoBehaviour
 
     private void Update()
     {
-        if (IsFadedToBlack())
+        if (inTransition)
+        {
+            UpdateForTransitionEffects();
+        }
+
+        if (IsFadedOut())
         {
             SceneManager.LoadScene(nextSceneName);
         }
     }
 
-    private bool IsFadedToBlack()
+    private void UpdateForTransitionEffects()
     {
-        return fullScreenBlackRectangle is Image && fullScreenBlackRectangle.canvasRenderer.GetAlpha() == 1;
+        var mainModule = TimeParticles.main;
+        mainModule.simulationSpeed += CalculateTransitionEffectValueDelta(FinalParticleSimulationSpeed, initialParticleSimulationSpeed);
+
+        var scale = TimeParticles.shape.scale;
+        scale.x += CalculateTransitionEffectValueDelta(FinalParticleXScale, initialParticleXScale);
+        scale.y += CalculateTransitionEffectValueDelta(FinalParticleYScale, initialParticleYScale);
+        var shape = TimeParticles.shape;
+        shape.scale = scale;
+
+        var titleLocalScale = Title.localScale;
+        titleLocalScale.x += CalculateTransitionEffectValueDelta(FinalTitleXScale, initialTitleXScale);
+        titleLocalScale.y += CalculateTransitionEffectValueDelta(FinalTitleYScale, initialTitleYScale);
+        Title.localScale = titleLocalScale;
+    }
+
+    private float CalculateTransitionEffectValueDelta(float final, float initial)
+    {
+        return (final - initial) / TransitionTimeInSeconds * Time.deltaTime;
+    }
+
+    private bool IsFadedOut()
+    {
+        return fullScreenRectangle is Image && fullScreenRectangle.canvasRenderer.GetAlpha() == 1;
     }
 
     private void TransitionToLevel(string sceneName, TimePeriod timePeriod)
@@ -40,18 +78,30 @@ public class LevelTransitionController : MonoBehaviour
         if (inTransition) return;
 
         inTransition = true;
+        PrepareForTransitionEffects();
         nextSceneName = sceneName;
         TimeCannonPlayerPrefs.SetTimePeriod(timePeriod);
-        FadeToBlack();
+        FadeOut();
     }
 
-    private void FadeToBlack()
+    private void PrepareForTransitionEffects()
+    {
+        initialParticleSimulationSpeed = TimeParticles.main.simulationSpeed;
+        initialParticleXScale = TimeParticles.shape.scale.x;
+        initialParticleYScale = TimeParticles.shape.scale.y;
+        initialTitleXScale = Title.localScale.x;
+        initialTitleYScale = Title.localScale.y;
+        var colorOverLifeTime = TimeParticles.colorOverLifetime;
+        colorOverLifeTime.enabled = false;
+    }
+
+    private void FadeOut()
     {
         GameObject blackRectangleContainer = new GameObject();
-        this.fullScreenBlackRectangle = blackRectangleContainer.AddComponent<Image>();
+        this.fullScreenRectangle = blackRectangleContainer.AddComponent<Image>();
         blackRectangleContainer.transform.SetParent(this.transform);
-        this.fullScreenBlackRectangle.color = new Color32(0, 0, 0, 255);
-        this.fullScreenBlackRectangle.canvasRenderer.SetAlpha(0);
+        this.fullScreenRectangle.color = new Color32(255, 255, 255, 255);
+        this.fullScreenRectangle.canvasRenderer.SetAlpha(0);
 
         var rectTransform = blackRectangleContainer.GetComponent<RectTransform>();
         rectTransform.anchorMin = new Vector2(0, 0);
@@ -60,6 +110,6 @@ public class LevelTransitionController : MonoBehaviour
         rectTransform.offsetMax = new Vector2(0, 0);
 
         blackRectangleContainer.SetActive(true);
-        fullScreenBlackRectangle.CrossFadeAlpha(1, TransitionTimeInSeconds, false);
+        fullScreenRectangle.CrossFadeAlpha(1, TransitionTimeInSeconds, false);
     }
 }
