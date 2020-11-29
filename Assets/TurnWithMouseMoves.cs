492c51cc981c338e6c7927350d7506357ce44494
Gamkedo <chris@gamkedo.com>
2020-11-28 20:39:40 -0800

    Adding a laser pointer for testing VR menu interaction outside of VR

---
+++ Assets/TurnWithMouseMoves.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnWithMouseMoves : MonoBehaviour
+{
+    float offX = 0.0f, offY = 0.0f;
+    void Update()
+    {
+        offX += 850.0f * Time.deltaTime * Input.GetAxisRaw("Mouse X");
+        offY += -650.0f * Time.deltaTime * Input.GetAxisRaw("Mouse Y");
+
+        transform.rotation = Quaternion.identity *
+            Quaternion.AngleAxis(offX, Vector3.up) *
+            Quaternion.AngleAxis(offY, Vector3.right);
+    }
+}
---
+++ Assets/lineForward.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineForward : MonoBehaviour
+{
+    LineRenderer lr;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, transform.position+transform.forward*10.0f);
+    }
+}
