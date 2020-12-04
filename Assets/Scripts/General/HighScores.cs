977dd192e11dd84f13de8616e8dbb23c2935a2b0
KornelGM <KORNik_k@wp.pl>
2020-12-04 22:11:23 +0100

    Added helper class for easy saving and loading of high scores

---
+++ Assets/Scripts/General/HighScores.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * Description: Saved and loads high scores.
+ * Authors: Kornel
+ * Copyright: © 2020 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+
+public static class HighScores
+{
+    /// <summary>
+    /// Gets high scores.
+    /// </summary>
+    /// <param name="levelName">Name of the level.</param>
+    /// <param name="mode">Name of game mode.</param>
+    /// <returns></returns>
+	public static int GetHighScore(string levelName, string mode)
+    {
+        return PlayerPrefs.GetInt(string.Concat(levelName, mode), 0);
+    }
+
+    /// <summary>
+    /// Saves high score.
+    /// </summary>
+    /// <param name="levelName">Name of the level.</param>
+    /// <param name="mode">Name of game mode.</param>
+    /// <param name="score">Score amount.</param>
+    public static void SaveHighScore(string levelName, string mode, int score)
+    {
+        PlayerPrefs.SetInt(string.Concat(levelName, mode), score);
+    }
+}
