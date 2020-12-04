0871836d09b3b9d30b98ab096b3c4e86f6cd4058
KornelGM <KORNik_k@wp.pl>
2020-12-04 22:26:36 +0100

    Added helper methods for even easier use of the hight scores saving/loading

--- Assets/Scripts/General/HighScores.cs
+++ Assets/Scripts/General/HighScores.cs
@@ -6,8 +6,33 @@
 
 using UnityEngine;
 
+public enum LevelName
+{
+    ChaosDimension,
+    City,
+    Countryside,
+    Ocean
+}
+
+public enum LevelMode
+{
+    TrackLocked,
+    FreePosition
+}
+
 public static class HighScores
 {
+    /// <summary>
+    /// Gets high scores.
+    /// </summary>
+    /// <param name="levelName">Name of the level.</param>
+    /// <param name="mode">Name of game mode.</param>
+    /// <returns></returns>
+    public static int GetHighScore(LevelName levelName, LevelMode mode)
+    {
+        return PlayerPrefs.GetInt(string.Concat(levelName.ToString(), mode.ToString()), 0);
+    }
+
     /// <summary>
     /// Gets high scores.
     /// </summary>
@@ -17,6 +42,17 @@ public static class HighScores
 	public static int GetHighScore(string levelName, string mode)
     {
         return PlayerPrefs.GetInt(string.Concat(levelName, mode), 0);
+    }
+
+    /// <summary>
+    /// Saves high score.
+    /// </summary>
+    /// <param name="levelName">Name of the level.</param>
+    /// <param name="mode">Name of game mode.</param>
+    /// <param name="score">Score amount.</param>
+    public static void SaveHighScore(LevelName levelName, LevelMode mode, int score)
+    {
+        PlayerPrefs.SetInt(string.Concat(levelName.ToString(), mode.ToString()), score);
     }
 
     /// <summary>
